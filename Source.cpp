b982716743854924624210a1042601f34fe46bac
MinCarve <artem.starikovich@gmail.com>
2017-07-26 19:27:24 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -135,7 +135,7 @@ int main()
 
 	random_device rd;
 
-	int x = rd() % 26;
+	int x = rd() % 32;
 
 
 	char letters[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
